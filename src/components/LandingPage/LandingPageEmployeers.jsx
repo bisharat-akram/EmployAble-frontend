322664b29feac513ad07688f0541cd8f66052fc0
Arzoo Khalil <arzookhalil7@gmail.com>
2024-01-13 01:32:35 +0500

    Add formspree

--- src/components/LandingPage/LandingPageEmployeers.jsx
+++ src/components/LandingPage/LandingPageEmployeers.jsx
@@ -1,16 +1,31 @@
 import { Data } from "../data";
 import Employeers from "../../assets/images/Employeers1.png";
+import { Button } from "antd";
+import { useNavigate } from "react-router";
 
 const LandingPageEmployeers = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="aboutPageStyle">
-        <div className="aboutUsHeadingStyle">Employeers</div>
+        <div className="aboutUsHeadingStyle">Employers</div>
         <div className="aboutUsDataContainerStyle">
-          <div className="aboutUsDataStyle">{Data.EmployersData}</div>
+          <div>
+            <div className="aboutUsDataStyle">{Data.EmployersData}</div>
+            <Button
+              className="LandingPageEmployerButtonStyle"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              {Data.EmployersButton}
+            </Button>
+          </div>
           <div>
             <img src={Employeers} className="teamworkImageStyle" />
           </div>
+          <div></div>
         </div>
       </div>
     </>
